d9e05c7c97da56f82d3ea0abf53d2a513614787e
VietNguyenSet1D <vnguyen141@my.bcit.ca>
2023-11-24 12:08:14 -0800

    Added Algorithm class

---
+++ Algorithm.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hyper on 2023-11-24.
+//
+
+#include "Algorithm.hpp"
---
+++ Algorithm.hpp
@@ -0,0 +1,16 @@
+//
+// Created by hyper on 2023-11-24.
+//
+
+#ifndef LAB1TEMPLATE_ALGORITHM_HPP
+#define LAB1TEMPLATE_ALGORITHM_HPP
+/**
+ *
+ */
+
+class Algorithm {
+
+};
+
+
+#endif //LAB1TEMPLATE_ALGORITHM_HPP
