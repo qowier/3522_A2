25f3317d2c522c352dda3433890c268fb227876b
Haurence Li <hli223@my.bcit.ca>
2023-11-24 12:17:04 -0800

    get/set for Tour.cpp

---
+++ Tour.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Haurence on 24/11/2023.
+//
+
+#include "Tour.hpp"
+
+double Tour::getFitnessRating() const {
+    return fitnessRating;
+}
+
+void Tour::setFitnessRating(double fitnessRating) {
+    Tour::fitnessRating = fitnessRating;
+}
